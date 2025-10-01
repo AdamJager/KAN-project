143d8bfb5f7cdb37512265cb52cab8959fa57a7c
AdamJager <a.jager-21@student.lboro.ac.uk>
2025-10-01 06:28:23 +0100

    cleaning up
    removed some old attempts of implementing saving/loading from files that aren't involved with the process anymore. These were commented out for a long time anyway so I removed them.

--- KAN_variants/ChebyKAN.py
+++ KAN_variants/ChebyKAN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import os
 
+# ChebyKANLayer class taken from 
 # This is inspired by Kolmogorov-Arnold Networks but using Chebyshev polynomials instead of splines coefficients
 class ChebyKANLayer(nn.Module):
     def __init__(self, input_dim, output_dim, degree):
@@ -9,6 +10,7 @@ class ChebyKANLayer(nn.Module):
         self.inputdim = input_dim
         self.outdim = output_dim
         self.degree = degree
+        self.alpha = nn.Parameter(torch.ones(1))
 
         self.cheby_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
         nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
@@ -17,7 +19,7 @@ class ChebyKANLayer(nn.Module):
     def forward(self, x):
         # Since Chebyshev polynomial is defined in [-1, 1]
         # We need to normalize x to [-1, 1] using tanh
-        x = torch.tanh(x)
+        x = torch.tanh(self.alpha*x)
         # View and repeat input degree + 1 times
         x = x.view((-1, self.inputdim, 1)).expand(
             -1, -1, self.degree + 1
@@ -35,49 +37,29 @@ class ChebyKANLayer(nn.Module):
         y = y.view(-1, self.outdim)
         return y
 
+    def plotNeurons(self, numPoints=1000):
+        # Create linspace to sample neurons
+        x = torch.linspace(-1, 1, numPoints)
+        
+        y = self.forward(x)
+
+        return x, y
 
     def setCoeffs(self, coeffs):
         self.cheby_coeffs = coeffs
 
+
 class ChebyKAN(nn.Module):
     def __init__(self, layers, degree):
         super(ChebyKAN, self).__init__()
-        #self.layers = [ChebyKANLayer(layers[i], layers[i+1], degrees) for i in range(len(layers) - 1)]
-        #self.normLayers = [nn.LayerNorm(layers[i+1]) for i in range(len(layers) - 2)]
+
 
         self.layers = nn.ModuleList()
         for i in range(len(layers) - 1):
             self.layers.append(ChebyKANLayer(layers[i], layers[i+1], degree))
 
-        self.normLayers = nn.ModuleList()
-        for i in range(len(layers) - 2):
-            self.normLayers.append(nn.LayerNorm(layers[i+1]))
-
 
     def forward(self, x):
-        for layer, normLayer in zip(self.layers, self.normLayers):
+        for layer in self.layers:
             x = layer(x)
-            x = normLayer(x)
-        x = self.layers[-1](x)
         return x
-    
-
-#    def saveModel(self, modelDirectory):
-#        if not os.path.isdir(modelDirectory):
-#            os.makedirs(modelDirectory)
-#        for index, layer in enumerate(self.layers):
-#            layerPath = modelDirectory + f"\\layer{index}.pt"
-#            torch.save(layer.cheby_coeffs, layerPath)
-#
-#    
-#    def loadModel(self, modelDirectory):
-#        if not os.path.exists(modelDirectory):
-#            print("This path does not exist")
-#            return
-#        
-#        if len(self.layers) != len(os.listdir(modelDirectory)):
-#            print(f"The loaded model is expecting {len(os.listdir(modelDirectory))} layers, you have {len(self.layers)}")
-#
-#        for index, fileName in enumerate(os.listdir(modelDirectory)):
-#            filePath = os.path.join(modelDirectory, fileName)
-#            self.layers[index].cheby_coeffs = torch.load(filePath)
